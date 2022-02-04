436c4696888f01a26bfceadd19828d54bab43b5a
Cláudio Santos <cljos@hotmail.com>
2022-02-03 23:38:10 -0300

    feat: create middleware

--- src/index.js
+++ src/index.js
@@ -27,17 +27,23 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-  const { cpf } = req.headers;
+app.get("/statement", verifyIfExistsAccountCpf, (req, res) => {
+  const { customer } = req;
+  return res.status(200).json(customer.statements);
+});
 
+function verifyIfExistsAccountCpf(req, res, next) {
+  const { cpf } = req.headers;
   const customer = customers.find((customer) => customer.cpf === cpf);
 
+  req.customer = customer;
+
   if (customer) {
-    return res.status(200).json(customer.statements);
+    return next();
   }
 
   return res.status(400).json({ error: "customer not found" });
-});
+}
 
 app.listen(3333, () => {
   console.log("Server listening on port 3333");
