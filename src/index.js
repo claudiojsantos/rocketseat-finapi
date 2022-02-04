a660fd4de079278152f63e788ce8877a13c792a6
Cláudio Santos <cljos@hotmail.com>
2022-02-03 22:51:37 -0300

    feat: create route post with validate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customer_exists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customer_exists) {
+    return res.status(400).json({ error: "customer already exists" });
+  }
+
+  const account = {
+    id: uuidv4(),
+    name,
+    cpf,
+    statements: [],
+  };
+
+  customers.push(account);
+
+  return res.status(201).send();
+});
 
 app.listen(3333, () => {
   console.log("Server listening on port 3333");
